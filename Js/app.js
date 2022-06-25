f168495a85016fea736b314a997f3b613e0388b1
Mritunjay1711 <mrityunjaycooldude2000@gmail.com>
2022-06-25 19:20:04 +0530

    new

--- Js/app.js
+++ Js/app.js
@@ -5,6 +5,21 @@ let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener('click', function(e){
     let addTxt = document.getElementById('addTxt');
     // console.log(addTxt.value);
+    let addTitle = document.getElementById('noteTitle');
+
+    //Fetching title from localStorage and adding the new title in the end and putting it back in storage
+    let title = localStorage.getItem("title");
+    if(title == null){
+        titleObj = [];
+    }
+    else{
+        titleObj = JSON.parse(title);
+    }
+    titleObj.push(addTitle.value);
+    localStorage.setItem("title", JSON.stringify(titleObj));
+    addTitle.value = "";
+
+    //Fetching the notes from localStorage and adding the new note in the end and putting it back in storage
     let notes = localStorage.getItem("notes");
     if(notes == null){
         notesObj = [];
@@ -22,6 +37,15 @@ addBtn.addEventListener('click', function(e){
 
 //Function to show notes from localStorage
 function showNotes(){
+    let title = localStorage.getItem("title");
+    if(title == null){
+        titleObj = [];
+    }
+    else{
+        titleObj = JSON.parse(title);
+    }
+
+
     let notes = localStorage.getItem("notes");
     if(notes == null){
         notesObj = [];
@@ -30,12 +54,13 @@ function showNotes(){
         notesObj = JSON.parse(notes);
     }
 
+
     let html = "";
     notesObj.forEach(function(element, index){
         html += `
         <div class="noteCard mx-2 my-2" style="width: 18rem;">
                 <div class="card-body">
-                  <h5 class="card-title">Note ${index + 1}</h5>
+                  <h5 class="card-title">${titleObj[index]}</h5>
                   <p class="card-text">${element}</p>
                   <button id = "${index}" onclick="deleteNote(this.id)"  class="btn btn-primary">Delete Note</button>
                 </div>
@@ -55,6 +80,16 @@ function showNotes(){
 //function to delete a note
 function deleteNote(index){
     // console.log('I am deleting', index);
+    let title = localStorage.getItem("title");
+    if(title == null){
+        titleObj = [];
+    }
+    else{
+        titleObj = JSON.parse(title);
+    }
+    titleObj.splice(index, 1);
+    localStorage.setItem('title', JSON.stringify(titleObj));
+
     let notes = localStorage.getItem("notes");
     if(notes == null){
         notesObj = [];
